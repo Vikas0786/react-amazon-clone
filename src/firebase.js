4b0f4d611c30b66da7d09b55d64a1480d5ada52b
Vikas0786 <vthakur412000@gmail.com>
2021-04-19 15:19:12 +0530

    Building Amazon Clone Starting with Header Section

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ9LR_aAv1svOTNbEu2YTMBdI0s2sUMWo",
+  authDomain: "challenge-f24bd.firebaseapp.com",
+  projectId: "challenge-f24bd",
+  storageBucket: "challenge-f24bd.appspot.com",
+  messagingSenderId: "557743187273",
+  appId: "1:557743187273:web:422e717565d23020fa6698",
+  measurementId: "G-QLTEJ6F0V4",
+};
