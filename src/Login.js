e60ca7fae489e1bdf496ce039b301fd1210066a8
Vikas0786 <vthakur412000@gmail.com>
2021-04-23 21:42:39 +0530

    Building Authentication using Firebase

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import { auth } from "./firebase";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth);
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -18,12 +37,26 @@ function Login() {
 
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signInButton">Sign In</button>
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
         </form>
         <p>
           By signing-in you agree to Amazon's Condition of Use & Sale. Please
@@ -31,7 +64,7 @@ function Login() {
           Notice.
         </p>
 
-        <button className="login__registerButton">
+        <button onclick={register} className="login__registerButton">
           Create your Amazon Account
         </button>
       </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDZ9LR_aAv1svOTNbEu2YTMBdI0s2sUMWo",
   authDomain: "challenge-f24bd.firebaseapp.com",
@@ -8,3 +9,10 @@ const firebaseConfig = {
   appId: "1:557743187273:web:422e717565d23020fa6698",
   measurementId: "G-QLTEJ6F0V4",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
